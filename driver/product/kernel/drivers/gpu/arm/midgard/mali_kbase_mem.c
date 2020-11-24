24bc92710efa3f92d0aa62a7de792ac5fe421b3c
Romain TISSERAND <romain.tisserand@gmail.com>
2020-11-24 15:55:19 +0100

    Fix stupid #else / #elif mistake

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_mem.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_mem.c
@@ -4321,7 +4321,7 @@ KERNEL_VERSION(4, 5, 0) > LINUX_VERSION_CODE
 			alloc->imported.user_buf.nr_pages,
 			reg->flags & KBASE_REG_GPU_WR ? FOLL_WRITE : 0,
 			pages, NULL);
-#else LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0)
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0)
 	pinned_pages = get_user_pages_remote(NULL, mm,
 			address,
 			alloc->imported.user_buf.nr_pages,
